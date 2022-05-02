c1b3d258e9e053ba78f634bf0599cd2a8ee9f5d3
Thomas Luechtefeld <tom@insilica.co>
2022-05-01 22:54:17 -0400

    changed lzy to lazy

--- packages/R/R/pull.R
+++ packages/R/R/pull.R
@@ -26,7 +26,7 @@ bake <- function(brick){
 
 # attempts to parse a data directory into dplyr lazy tables
 #' @export
-lzy <- function(files){
+lazy <- function(files){
   pqt <- files |> purrr::keep(~ fs::path_ext(.) == "parquet")
   nm  <- fs::path_file(pqt) |> fs::path_ext_remove()
   purrr::map(pqt,~arrow::read_parquet(.,as_data_frame=F)) |> purrr::set_names(nm)
