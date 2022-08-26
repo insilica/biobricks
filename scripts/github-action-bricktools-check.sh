892b2adef435d7958245cb9d66a7528016919efe
Thomas Luechtefeld <tom@insilica.co>
2022-08-26 11:06:37 -0400

    docker image 3.0 and latest

--- scripts/github-action-bricktools-check.sh
+++ scripts/github-action-bricktools-check.sh
@@ -5,3 +5,4 @@ echo CHECKING https://github.com/$GITHUB_REPOSITORY
 git clone https://github.com/$GITHUB_REPOSITORY $GITHUB_REPOSITORY
 cd $GITHUB_REPOSITORY
 Rscript -e 'bricktools::check(".")'
+echo SUCCESS!
